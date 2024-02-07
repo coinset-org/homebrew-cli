a7edc6738ab2c86516748c8ac3ab2731655b1f0d
Cameron Cooper <cameron@coinset.org>
2024-02-07 11:05:52 -0600

    Brew formula update for coinset version v0.0.6

--- coinset.rb
+++ coinset.rb
@@ -5,54 +5,62 @@
 class Coinset < Formula
   desc "CLI for accessing the Chia blockchain."
   homepage "https://www.coinset.org/"
-  version "0.0.5"
+  version "0.0.6"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/coinset-org/cli/releases/download/v0.0.5/coinset_Darwin_arm64.tar.gz"
-      sha256 "b3432c454d1b5671722300bd5db3c0b2a2f00205d4d37a3bc5b25c0e4f540213"
+      url "https://github.com/coinset-org/cli/releases/download/v0.0.6/coinset_Darwin_arm64.tar.gz"
+      sha256 "40e6499c66dee984a3ff5a5c5fe63edf7a605d6a5c74337b5920e1b892f90545"
 
       def install
-        (bash_completion/"${BINARY_NAME}").write output
-        output = Utils.popen_read("SHELL=zsh #{bin}/${BINARY_NAME} completion zsh")
-        (zsh_completion/"_${BINARY_NAME}").write output
+        bin.install "coinset" => "coinset"
+        output = Utils.popen_read("SHELL=bash #{bin}/coinset completion bash")
+        (bash_completion/"coinset").write output
+        output = Utils.popen_read("SHELL=zsh #{bin}/coinset completion zsh")
+        (zsh_completion/"_coinset").write output
         prefix.install_metafiles
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/coinset-org/cli/releases/download/v0.0.5/coinset_Darwin_x86_64.tar.gz"
-      sha256 "8c54087262336eb41d0183a426a4e58c3f273f2bee83d1ae1d1034435b1a3eb1"
+      url "https://github.com/coinset-org/cli/releases/download/v0.0.6/coinset_Darwin_x86_64.tar.gz"
+      sha256 "5114c8679e370638101a6db00b53fa24d92c64edd2fedb942b46f37aa3f21ef3"
 
       def install
-        (bash_completion/"${BINARY_NAME}").write output
-        output = Utils.popen_read("SHELL=zsh #{bin}/${BINARY_NAME} completion zsh")
-        (zsh_completion/"_${BINARY_NAME}").write output
+        bin.install "coinset" => "coinset"
+        output = Utils.popen_read("SHELL=bash #{bin}/coinset completion bash")
+        (bash_completion/"coinset").write output
+        output = Utils.popen_read("SHELL=zsh #{bin}/coinset completion zsh")
+        (zsh_completion/"_coinset").write output
         prefix.install_metafiles
       end
     end
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/coinset-org/cli/releases/download/v0.0.5/coinset_Linux_x86_64.tar.gz"
-      sha256 "8108ce3b8d0a938c8a4dd348b6e1273179221df23dbe3a50ce661fc74215b6c1"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/coinset-org/cli/releases/download/v0.0.6/coinset_Linux_arm64.tar.gz"
+      sha256 "805e16fca46c4061557d66852a3d4a64da0f5b630664fc1f0e984eabc77f4be6"
 
       def install
-        (bash_completion/"${BINARY_NAME}").write output
-        output = Utils.popen_read("SHELL=zsh #{bin}/${BINARY_NAME} completion zsh")
-        (zsh_completion/"_${BINARY_NAME}").write output
+        bin.install "coinset" => "coinset"
+        output = Utils.popen_read("SHELL=bash #{bin}/coinset completion bash")
+        (bash_completion/"coinset").write output
+        output = Utils.popen_read("SHELL=zsh #{bin}/coinset completion zsh")
+        (zsh_completion/"_coinset").write output
         prefix.install_metafiles
       end
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/coinset-org/cli/releases/download/v0.0.5/coinset_Linux_arm64.tar.gz"
-      sha256 "9b508ad66b0179d1abe4aeaa92cbca38e369cae21d220a7c831e30fbd31c6784"
+    if Hardware::CPU.intel?
+      url "https://github.com/coinset-org/cli/releases/download/v0.0.6/coinset_Linux_x86_64.tar.gz"
+      sha256 "2c669b295b9b2ac023aa23df0d32a7ee402c88f17ada858871e6c40612fe99e4"
 
       def install
-        (bash_completion/"${BINARY_NAME}").write output
-        output = Utils.popen_read("SHELL=zsh #{bin}/${BINARY_NAME} completion zsh")
-        (zsh_completion/"_${BINARY_NAME}").write output
+        bin.install "coinset" => "coinset"
+        output = Utils.popen_read("SHELL=bash #{bin}/coinset completion bash")
+        (bash_completion/"coinset").write output
+        output = Utils.popen_read("SHELL=zsh #{bin}/coinset completion zsh")
+        (zsh_completion/"_coinset").write output
         prefix.install_metafiles
       end
     end
